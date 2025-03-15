410edf83ec479307dfd2a61647d200ccdd68ee41
andrei_g_git <andrei_g_git@protonmail.com>
2025-03-15 18:09:17 +0200

    cleanup unused methods in grid controller and model

--- scripts/grid/controller/grid.gd
+++ scripts/grid/controller/grid.gd
@@ -46,7 +46,7 @@ func _process(delta: float) -> void:
 
 func _spawn_pieces():#pieces_in_grid: Array[Array]):	
 	var pieces_in_grid = model.create_grid()
-	print_array_initials(pieces_in_grid, "TEST NEW MODEL SPAWN")
+	GridUtilities.print_array_initials(pieces_in_grid, "TEST NEW MODEL SPAWN")
 	_populate_and_register_observers(pieces_in_grid, piecessss, cols, rows)
 
 
@@ -91,42 +91,6 @@ func _populate_grid_with_nodes(grid : Array[Array], possible_pieces : Array[Reso
 	return instance_grid
 
 
-func _choose_random_piece(pieces_: Array[Resource]):
-	var random = floor(randi_range(0, pieces_.size() - 1))
-	var piece : Control = (pieces_[random] as Resource).instantiate()
-	#print(piece)
-	return piece
-
-
-func _is_match_at(x: int, y: int, pieces_in_grid: Array[Array], piece_name: String):
-	#print("current: \n %s \n last: \n %s \n 2nd last: \n %s" % [piece_name, pieces_in_grid[x-1][y], pieces_in_grid[x-2][y]])
-	if(
-		#x > 1 and 
-		#pieces_in_grid[x] \
-			#.slice(-2, pieces_in_grid[x].size()) \
-			#.has(piece)			
-		y > 1 and 
-		pieces_in_grid[x-1][y] != "" and #don't need this if I compare strings, the current piece will never be empty string
-		pieces_in_grid[x-2][y] != "" 				
-	):
-		if(
-			pieces_in_grid[x-1][y] == piece_name and 
-			pieces_in_grid[x-2][y] == piece_name		
-						
-		):
-			return true
-	if(
-		y > 1 and 
-		pieces_in_grid[x][y-1] != "" and 
-		pieces_in_grid[x][y-2] != ""
-	):
-		if(
-			pieces_in_grid[x][y-1] == piece_name and 
-			pieces_in_grid[x][y-2] == piece_name			
-		):
-			return true
-	return false
-	
 func _handle_possible_swap(tile: BaseButton, tiles: Array[Array], tile_resources: Array[Resource], tile_name: String):
 	var direction : Vector2i = tile.get_drag_direction()
 	var source : Vector2i = tile.get_grid_index()
@@ -138,70 +102,4 @@ func _handle_possible_swap(tile: BaseButton, tiles: Array[Array], tile_resources
 	)
 
 
-func _swap_2_tiles(
-	tiles: Array[Array],
-	pressed_tile: BaseButton,
-	source: Vector2i,
-	destination: Vector2i,
-	direction: Vector2i,
-	tile_name: String
-):
-	if(destination.x >= 0 and destination.y >= 0):
-		var new_board: Array[Array] = tiles.duplicate(true)
-		var tile_to_swap = new_board[destination.x][destination.y]
-		new_board[destination.x][destination.y] = tile_name#pressed_tile.name.replace("&", "")
-		new_board[source.x][source.y] = tile_to_swap	
-		return new_board
-	return []
-
-#this only finds 1 matching tile, not 3 or more, because it uses _is_match_at which only registers a match once and so only 1 tile will be stored in the matching tile list even if there are 3+
-func _remove_matches(tiles: Array[Array]) -> Array[Array]:
-	var rows = tiles.size()
-	var cols = tiles[0].size() 
-	var matching_tile_indexes : Array[Vector2i] = []
-	for x in rows:
-		for y in cols:
-			if _is_match_at(x, y, tiles, tiles[x][y]):
-				matching_tile_indexes.append(Vector2i(x, y))
-	for a in rows:
-		for b in cols:
-			for c in matching_tile_indexes.size():
-				if(a == matching_tile_indexes[c].x and b == matching_tile_indexes[c].y):
-					tiles[a][b] = ""
-	return tiles
-
-#might be better to work with instance array than name arrays? -- at least switch to enums or something...
-func _populate_grid(tiles: Array[Array], tile_resources: Array[Resource]):
-	var rows = tiles.size()
-	var cols = tiles[0].size()
-	var resources = tile_resources.size()
-	
-	print_array_initials(tiles, "altered-two")
-	
-	print("\n\n######################\n")
-
-	for x in rows: #gotta iterate grid first and resources last otherwise it populates with only 3 pieces
-		for y in cols:
-			for z in resources:
-				var instance_ : Control = (tile_resources[z] as Resource).instantiate()
-				var name_ = instance_.name.replace("&", "")
-				if name_ == tiles[x][y]:
-					print_array_initials(tiles, "x, y    " + str(x) + "  " + str(y))
-					#await get_tree().create_timer(0.1).timeout
-					add_child(instance_)
-
-
-
-func print_array_initials(tiles, header: String):
-
-	var intialsGrid: Array[Array] = []
-
-	intialsGrid.resize(rows)
-
-	print(header, "\n")
-	for i in rows:
-		if tiles[i].size():
-			intialsGrid[i] = tiles[i].map(func (item): return item[0]) #get first letter		
-			print(intialsGrid[i])
-	print("\n")
 		
--- scripts/grid/model/grid.gd
+++ scripts/grid/model/grid.gd
@@ -43,27 +43,20 @@ func create_grid():
 	_new_grid = _current_grid.duplicate(true)
 	return _current_grid
 
+
 func _is_match_at(x: int, y: int, tiles: Array[Array], tile_name: String):
-	if(			
-		x > 1 #and          #I DON"T THINK I NEED THESE CHECKS, matching empty tiles doesn't change anything, they still get replaced by empty tiles after swapping
-		#tiles[x-1][y] != "" and 
-		#tiles[x-2][y] != "" 				
-	):
-		if(
+	if( 
+		(
+			x > 1 and
 			tiles[x-1][y] == tile_name and 
-			tiles[x-2][y] == tile_name			
-		):
-			return true
-	if(
-		y > 1 #and 
-		#tiles[x][y-1] != "" and 
-		#tiles[x][y-2] != ""
-	):
-		if(
+			tiles[x-2][y] == tile_name
+		) or (
+			y > 1 and
 			tiles[x][y-1] == tile_name and 
-			tiles[x][y-2] == tile_name			
-		):
-			return true
+			tiles[x][y-2] == tile_name					
+		)			
+	):
+		return true
 	return false
 
 
@@ -121,10 +114,8 @@ func swap_2(
 func _swap_tile_nodes(source: Vector2i, destination: Vector2i):
 	var source_node : Control = tile_nodes[source.x][source.y]
 	var destination_node : Control = tile_nodes[destination.x][destination.y]
-	#_print_grid_coords("@@@@@@@@@@@")
 	source_node.update(destination)
 	destination_node.update(source)
-	#_print_grid_coords("%%%%%%%%%%%%")	
 
 
 func _remove_matches(tile_to_match: String): 
@@ -203,31 +194,18 @@ func _find_linear_matches(tile_to_match: String, horizontally: bool):
 					matches.append(Vector2i(x, y))
 					matches.append(Vector2i(x, y + 1))
 	return Collections.remove_array_duplicates(matches)	
-				
-
-
-func _find_matches():
-	pass
-
-
-#will probably use flood fill
-func _remove_matches__nahhhhhh_____(tiles: Array[Array]) -> Array[Array]:
-	var matching_tile_indexes : Array[Vector2i] = []
-	for x in rows:
-		for y in columns:
-			if _is_match_at(x, y, tiles, tiles[x][y]):
-				matching_tile_indexes.append(Vector2i(x, y))   #this only detects the last tile that matches and will only remove that piece and leave the 2 matching tiles 'behind' it alone
-	print("matching tile indexes:  \n", matching_tile_indexes)
-	for a in rows:
-		for b in columns:
-			for c in matching_tile_indexes.size():
-				if(a == matching_tile_indexes[c].x and b == matching_tile_indexes[c].y):
-					tiles[a][b] = "" #this seems to remove too few pieces, maybe fewer by 2
-	return tiles
 
 
 func get_grid():
 	return _new_grid
+
+
+
+
+
+
+
+
 
 
 func _print_grid_coords(header: String):
