62ee844b2e5893990e5460f56b51f3d2c905927d
andrei_g_git <andrei_g_git@protonmail.com>
2025-05-31 18:31:54 +0300

    add step and attack ability to fighter tile, buggy

--- features/tiles/variants/fighter/Manager.cs
+++ features/tiles/variants/fighter/Manager.cs
@@ -1,3 +1,4 @@
+using Abstractions;
 using Godot;
 using System;
 
@@ -11,12 +12,21 @@ namespace Fighter{
             //(model as Defensive.Model).ConnectTookDamage((defenseAnimator as Defensive.View).AnimateMethod);
             (model as Defensive.Model).ConnectTookDamage((defenseAnimator as Defensive.View).AnimateDefending);
 
+            var attackAnimator = GetNode<Node>("%Attack") as Box;
+            attackAnimator.Width = SideLength;
+            attackAnimator.Height = SideLength;
+            attackAnimator.Margin = Margin;
+
             var damageNumber = GetNode<Node>("%DamageNumber");
             (model as Defensive.Model).ConnectTookDamage((damageNumber as DisplayableNumber).DisplayNumberAt);
 
             (defenseAnimator as Box).Width = SideLength;
             (defenseAnimator as Box).Height = SideLength;
             (defenseAnimator as Box).Margin = Margin;
+
+            var model2 = (GetParent().GetParent() as TacticalGrid).Model;
+            var movement = GetNode<Node>("%Movement") as Updating.Model;
+            movement.ConnectUpdate(model2.Notify);
         }
     }
 }
